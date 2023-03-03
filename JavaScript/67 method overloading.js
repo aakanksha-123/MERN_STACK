05d7eccad1e4ef8bef3dd5618fb204db07a784cd
aakanksha-123 <94225817+aakanksha-123@users.noreply.github.com>
2023-03-04 00:54:30 +0530

    method overloading

---
+++ JavaScript/67 method overloading.js
@@ -0,0 +1,9 @@
+let a=100
+let b=200
+let c=a+b
+console.log(c)
+
+let x="Hello"
+let y="world"
+let z=x+y
+console.log(z)
--- JavaScript/72 modules demo/mymodule.js
+++
@@ -1,4 +0,0 @@
-export function sayHello(name){
-    alert(`Hello ${name}`)
-}
-
